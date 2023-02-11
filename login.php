23b7061b9b21daf58ad8b3e825777a4a2d6fbc27
samueltexa <woolardsamuels@gmail.com>
2023-02-12 01:17:11 +0300

    adding the login page

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>StockManager</title>
+		<link rel="stylesheet" href="./custom/custom.css">
+		<script src="custom/custom.js"></script>
+	</head>
+	<body>
+		<meta name="viewport"
+		content="=width=device-width,
+		initial-scale=1.0">
+		<form>
+	<div class="account-display">ACCOUNT</div>
+    	<div>
+      		<canvas class="canva" width="250" height="200"></canvas>
+    	</div>
+		<div class="container">
+			<label>Email</label>
+			<input type="text" placeholder="Enter email" name="email" required>
+			<label class="labels">Password</label>
+			<input type="Password" placeholder="Input Password" name="Password" required>
+		 	<button type="submit" class="btnlogin">Login</button>
+			<p>Forgot<span><a href="#"> password? </a></span></p>
+			<h2 class="reg">or</h2>
+			<div class="line"></div>
+			<a class="reg"href="#">Register</a>
+		</div>
+	</form>
+	</body>
+<html>
