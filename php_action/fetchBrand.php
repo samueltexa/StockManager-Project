9642b17a3e44fda500e3eba76616f5d51c8266f0
samueltexa <woolardsamuels@gmail.com>
2023-06-25 00:44:37 +0300

    edit order

--- php_action/fetchBrand.php
+++ php_action/fetchBrand.php
@@ -1,27 +1,48 @@
-<?php
-require "db_connection.php";
+<?php 	
+
+require_once 'core.php';
+
 $sql = "SELECT brand_id, brand_name, brand_active, brand_status FROM brands WHERE brand_status = 1";
 $result = $connect->query($sql);
-if ($result->num_rows > 0) {
-    echo "<table>";
-    echo "<tr>";
-    echo "<th>Brand Name</th>";
-    echo "<th>Status</th>";
-    echo "</tr>";
-	while($row = $result->fetch_array()) {
-		$productId = $row[0];
-		$brand_name = $row[1];
-		$brand_status = $row[3];
-		$class = $brand_status == 1 ? "available" : "unavailable";
-		// echo the table row
-		  echo "<tr>";
-		  echo "<td>" . $brand_name . "</td>";
-		  echo "<td class='" . $class . "'>" . ($brand_status == 1 ? "Available" : "Unavailable") . "</td>";
-		  echo "</tr>";
-	  }
-echo "</table>";
-} else {
-echo "No product Brand to display, add product brand";
-}
+
+$output = array('data' => array());
+
+if($result->num_rows > 0) { 
+
+ // $row = $result->fetch_array();
+ $activeBrands = ""; 
+
+ while($row = $result->fetch_array()) {
+ 	$brandId = $row[0];
+ 	// active 
+ 	if($row[2] == 1) {
+ 		// activate member
+ 		$activeBrands = "<label class='label label-success'>Available</label>";
+ 	} else {
+ 		// deactivate member
+ 		$activeBrands = "<label class='label label-danger'>Not Available</label>";
+ 	}
+
+ 	$button = '<!-- Single button -->
+	<div class="btn-group">
+	  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	    Action <span class="caret"></span>
+	  </button>
+	  <ul class="dropdown-menu">
+	    <li><a type="button" data-toggle="modal" data-target="#editBrandModel" onclick="editBrands('.$brandId.')"> <i class="glyphicon glyphicon-edit"></i> Edit</a></li>
+	    <li><a type="button" data-toggle="modal" data-target="#removeMemberModal" onclick="removeBrands('.$brandId.')"> <i class="glyphicon glyphicon-trash"></i> Remove</a></li>       
+	  </ul>
+	</div>';
+
+ 	$output['data'][] = array( 		
+ 		$row[1], 		
+ 		$activeBrands,
+ 		$button
+ 		); 	
+ } // /while 
+
+} // if num_rows
+
 $connect->close();
-//print_r($_POST);
+
+echo json_encode($output);
--- php_action/fetchProduct.php
+++ php_action/fetchProduct.php
@@ -1,46 +1,83 @@
-<?php
-require "db_connection.php";
+<?php 	
+
+
+
+require_once 'core.php';
+
 $sql = "SELECT product.product_id, product.product_name, product.product_image, product.brand_id,
- 		product.categories_id, product.quantity, product.rate, product.active, product.status,
- 		brands.brand_name, categories.categories_name FROM product
-		INNER JOIN brands ON product.brand_id = brands.brand_id
-		INNER JOIN categories ON product.categories_id = categories.categories_id
+ 		product.categories_id, product.quantity, product.rate, product.active, product.status, 
+ 		brands.brand_name, categories.categories_name FROM product 
+		INNER JOIN brands ON product.brand_id = brands.brand_id 
+		INNER JOIN categories ON product.categories_id = categories.categories_id  
 		WHERE product.status = 1";
+
 $result = $connect->query($sql);
-if ($result->num_rows > 0) {
-    echo "<table>";
-    echo "<tr>";
-    echo "<th>Product Image</th>";
-    echo "<th>Product Name</th>";
-    echo "<th>Rate</th>";
-    echo "<th>Quantity</th>";
-    echo "<th>Brand</th>";
-    echo "<th>Category</th>";
-    echo "<th>Status</th>";
-    echo "</tr>";
-            while($row = $result->fetch_array()) {
-                $productId = $row[0];
-                $productImage = $row[2];
-                $product_name = $row[1];
-                $rate = $row[6];
-                $quantity = $row[5];
-                $brand = $row[9];
-                $category = $row[10];
-                $status = $row[8];
-                $class = $status == 1 ? "available" : "unavailable";
-                // echo the table row
-                echo "<tr>";
-                echo "<td><img src='" . $productImage . "' alt='image cannot be displayed" . $product_name . "' style='width:100px;height:100px;'></td>";
-                echo "<td>" . $product_name . "</td>";
-                echo "<td>" . $rate . "</td>";
-                echo "<td>" . $quantity . "</td>";
-                echo "<td>" . $brand . "</td>";
-                echo "<td>" . $category . "</td>";
-                echo "<td class='" . $class . "'>" . ($status == 1 ? "Available" : "Unavailable") . "</td>";
-                echo "</tr>";
-            }
-    echo "</table>";
-} else {
-    echo "There is currently no products to display, add new products";
-}
+
+$output = array('data' => array());
+
+if($result->num_rows > 0) { 
+
+ // $row = $result->fetch_array();
+ $active = ""; 
+
+ while($row = $result->fetch_array()) {
+ 	$productId = $row[0];
+ 	// active 
+ 	if($row[7] == 1) {
+ 		// activate member
+ 		$active = "<label class='label label-success'>Available</label>";
+ 	} else {
+ 		// deactivate member
+ 		$active = "<label class='label label-danger'>Not Available</label>";
+ 	} // /else
+
+ 	$button = '<!-- Single button -->
+	<div class="btn-group">
+	  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	    Action <span class="caret"></span>
+	  </button>
+	  <ul class="dropdown-menu">
+	    <li><a type="button" data-toggle="modal" id="editProductModalBtn" data-target="#editProductModal" onclick="editProduct('.$productId.')"> <i class="glyphicon glyphicon-edit"></i> Edit</a></li>
+	    <li><a type="button" data-toggle="modal" data-target="#removeProductModal" id="removeProductModalBtn" onclick="removeProduct('.$productId.')"> <i class="glyphicon glyphicon-trash"></i> Remove</a></li>       
+	  </ul>
+	</div>';
+
+	// $brandId = $row[3];
+	// $brandSql = "SELECT * FROM brands WHERE brand_id = $brandId";
+	// $brandData = $connect->query($sql);
+	// $brand = "";
+	// while($row = $brandData->fetch_assoc()) {
+	// 	$brand = $row['brand_name'];
+	// }
+
+	$brand = $row[9];
+	$category = $row[10];
+
+	$imageUrl = substr($row[2], 3);
+	$productImage = "<img class='img-round' src='".$imageUrl."' style='height:30px; width:50px;'  />";
+
+ 	$output['data'][] = array( 		
+ 		// image
+ 		$productImage,
+ 		// product name
+ 		$row[1], 
+ 		// rate
+ 		$row[6],
+ 		// quantity 
+ 		$row[5], 		 	
+ 		// brand
+ 		$brand,
+ 		// category 		
+ 		$category,
+ 		// active
+ 		$active,
+ 		// button
+ 		$button 		
+ 		); 	
+ } // /while 
+
+}// if num_rows
+
 $connect->close();
+
+echo json_encode($output);
