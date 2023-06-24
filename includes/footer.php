1a640d1e57e49af306dd3579618c809a2c0fddfe
samueltexa <woolardsamuels@gmail.com>
2023-06-25 00:28:24 +0300

    footer

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+<?php require_once 'php_action/core.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Stock Management System</title>
+    <link rel="stylesheet" href="./custom/css/custom.css">
+</head>
+<body>
+	<div class="footer">
+		<ul class="ul">
+			<li><a href="#">About us</a></li>
+			<li><a href="#">Contact us</a></li>
+			<li><a href="#">@Copyright</a></li>
+       		<li><a href="#">Software By SamuelTech</a></li>
+		</ul>
+  </div>
+	</body>
+</html>
